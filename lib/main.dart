3f563d084990814646b16a1623249126986d6439
Samir <samirperezb1004@gmail.com>
2023-03-11 19:10:24 -0500

    created basic login screen (Harrison)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'activity.dart';
+import 'dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,203 +18,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
       routes: {
+        '/dashboard': (context) => const Dashboard(),
         '/activity': (context) => const FirstRoute(),
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Align(
-            alignment: Alignment.topLeft,
-            widthFactor: 1.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Hola, Juan',
-                  style: TextStyle(fontSize: 40),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/fredbear.png"),
+                  fit: BoxFit.cover,
                 ),
-                Text(
-                  '¿Que harás hoy?',
-                  style: TextStyle(fontSize: 20),
-                )
-              ],
-            )),
-        Align(
-            alignment: Alignment.center,
-            heightFactor: 2,
-            child: Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                Card(
-                  title: 'Bicicleta',
-                  image: Image.asset(
-                    'assets/images/bikeEmoji.png',
-                    width: 100,
-                    height: 100,
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: const <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Correo electrónico',
+                    ),
                   ),
-                  handleTab: () => Navigator.pushNamed(context, '/activity'),
+                  SizedBox(height: 20.0),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Contraseña',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/dashboard");
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+              ),
+              child: const Text(
+                'Iniciar sesión',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
                 ),
-                // Card(
-                //   title: 'Caminata',
-                //   image: Image.asset(
-                //     'assets/images/walkingEmoji.png',
-                //     width: 100,
-                //     height: 100,
-                //   ),
-                // ),
-                // Card(
-                //   title: 'Trotar',
-                //   image: Image.asset(
-                //     'assets/images/runningEmoji.png',
-                //     width: 100,
-                //     height: 100,
-                //   ),
-                // ),
-              ],
-            )),
-        Align(
-            alignment: Alignment.center,
-            child: Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                RoundButton(
-                    color: Colors.blue.shade300,
-                    icon: const Icon(
-                      CupertinoIcons.graph_circle,
-                      size: 40,
-                    )),
-                RoundButton(
-                    color: Colors.green.shade300,
-                    icon: const Icon(CupertinoIcons.clock)),
-                RoundButton(
-                    color: Colors.orange.shade300,
-                    icon: const Icon(CupertinoIcons.list_bullet)),
-                RoundButton(
-                    color: Colors.purple.shade300,
-                    icon: const Icon(CupertinoIcons.map)),
-              ],
-            ))
-      ],
-    ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
-  }
-}
-
-class RoundButton extends StatelessWidget {
-  final Color color;
-  final Icon icon;
-  const RoundButton({
-    super.key,
-    required this.color,
-    required this.icon,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      width: 60,
-      child: Material(
-        borderRadius: BorderRadius.circular(100),
-        color: color,
-        child: InkWell(onTap: () {}, child: icon),
-      ),
-    );
-  }
-}
-
-class Card extends StatelessWidget {
-  final String title;
-  final Image image;
-  final VoidCallback handleTab;
-
-  const Card({
-    super.key,
-    required this.title,
-    required this.image,
-    required this.handleTab,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.grey,
-      borderRadius: BorderRadius.circular(10),
-      child: InkWell(
-        onTap: handleTab,
-        child: Container(
-            height: 200,
-            width: 150,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  title,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                'Registrarse',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
                 ),
-                image
-              ],
-            )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
