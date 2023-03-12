ad7f8339ad93b430903f99bb1aa674c33f4ad25c
Samir <samirperezb1004@gmail.com>
2023-03-11 20:21:38 -0500

    finished basic layout for the activity screen

--- lib/activity.dart
+++ lib/activity.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FirstRoute extends StatelessWidget {
   const FirstRoute({super.key});
@@ -7,21 +8,91 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Align(
-            alignment: Alignment.topLeft,
-            widthFactor: 1.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Bicicleta',
-                  style: TextStyle(fontSize: 40),
-                ),
-              ],
-            )),
-      ],
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: <Widget>[
+          Align(
+              alignment: Alignment.center,
+              widthFactor: 1.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(24),
+                          elevation: 0),
+                      onPressed: () {},
+                      child: const Icon(CupertinoIcons.arrow_left_circle)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: <Widget>[
+                          const Text("Bicicleta"),
+                          const Text("Tiempo"),
+                          Container(
+                            alignment: Alignment.center,
+                            width: 80,
+                            color: Colors.grey,
+                            child: const Text("00:00:00"),
+                          ),
+                          const Text("Distancia"),
+                          Container(
+                            alignment: Alignment.center,
+                            width: 80,
+                            color: Colors.grey,
+                            child: const Text("0 m"),
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        "assets/images/bikeEmoji.png",
+                        width: 100,
+                        height: 100,
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Recorrido"),
+                        Container(
+                          width: double.infinity,
+                          height: 600,
+                          color: Colors.blue,
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        minimumSize: const Size.fromHeight(50),
+                        elevation: 0,
+                      ),
+                      onPressed: () {},
+                      child: const Text("Iniciar"),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      minimumSize: const Size.fromHeight(50),
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Eliminar"),
+                  ),
+                ],
+              )),
+        ],
+      ),
     ) // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
